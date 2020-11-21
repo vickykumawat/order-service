dda2b7ecb48f08e211f90dafb0ca23ac86e701dc
Vicky Kumawat <vickykumawat766@gmail.com>
2020-11-21 12:19:49 +0530

    adding order

--- src/main/java/com/javatechie/aws/cicd/example/OrderDao.java
+++ src/main/java/com/javatechie/aws/cicd/example/OrderDao.java
@@ -15,7 +15,7 @@ public class OrderDao {
                 new Order(101, "Mobile", 1, 30000),
                 new Order(58, "Book", 4, 2000),
                 new Order(205, "Laptop", 1, 150000),
-                new Order(809, "headset", 1, 1799))
+                new Order(809, "headset", 1, 1799),
                 new Order(809, "Mouse", 1, 500))
                 .collect(Collectors.toList());
     }
